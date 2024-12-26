48d04c75800208947e4ffaf2b198265a57e7f4d7
jiyadua <150153827+jiyadua@users.noreply.github.com>
2024-12-26 11:00:20 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 5678
+let accountEmail =  "jiyadua90@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+accountEmail="jiya123@gmail.com"
+accountPassword="34566"
+accountCity="gurgaon"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+1.Prefer not to use var
+because of block scope and functional scope
+2.does not change const variable
+
+*/
