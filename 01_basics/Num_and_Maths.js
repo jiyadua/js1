32d532beff65fe9b3483fedbfcb31ba40b3c4e2a
jiyadua <150153827+jiyadua@users.noreply.github.com>
2024-12-29 06:57:11 +0000

    commit

---
+++ 01_basics/Num_and_Maths.js
@@ -0,0 +1,38 @@
+const balance =400
+// console.log(balance)
+
+
+const num = new Number(780)
+// console.log(num)
+// console.log(num.toString().length)
+// console.log(num.toFixed(4))
+const otherNumber = 3.8966
+// console.log(otherNumber.toPrecision(3))
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// const name = new String("garv")
+// console.log(name)
+
+// ++++++++++++++++Maths++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(7.8));
+// console.log(Math.ceil(7.8));
+// console.log(Math.floor(7.8));
+// console.log(Math.sqrt(81));
+// console.log(Math.max(4,5,9,1));
+// console.log(Math.min(7,6,2,3));
+console.log(Math.random());//value lies between 0 and 1
+console.log(Math.floor(Math.random()*5) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
