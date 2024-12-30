0929cb90656092560ef38195a19c5df2e7f18f99
jiyadua <150153827+jiyadua@users.noreply.github.com>
2024-12-30 07:42:26 +0000

    commit

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,21 @@
+// destructuring 
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+// course.courseInstructor
+const {courseInstructor:instructor}=course
+console.log(instructor)
+
+// API
+{
+
+}
+[
+    {},
+    {},
+    {}
+]
