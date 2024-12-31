baf9fb8009f82bf9ff9c96129f69936dbb8eb709
jiyadua <150153827+jiyadua@users.noreply.github.com>
2024-12-31 14:01:19 +0000

    .

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,9 +1,35 @@
-if(true){
-    let a=10
-    const b=20
-    c=30
-    console.log("INNER:",a)
+// if(true){
+//     let a=10
+//     const b=20
+//     c=30
+//     console.log("INNER:",a)
+// }
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// function one(){
+//     const username = "jiya"
+//     function two(){
+//         const website = "youtube"
+        // console.log(username);
+        // console.log(website);
+
+    // }
+    // two();
+// }
+// one();
+/*++++++++++++++++++++++++Interesting+++++++++++++++++++++++++++++*/
+
+addone(5);
+
+function addone(num){
+return num+1;
 }
-console.log(a)
-console.log(b)
-console.log(c)
+addTwo(5);//generate error
+
+const  addTwo=function(num){
+    return num+2;
+}
+// addone();
+// addTwo(5);
