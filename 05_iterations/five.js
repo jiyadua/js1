fb5f08309efa5f50d75d5f3704e6a5fed5088fae
jiyadua <150153827+jiyadua@users.noreply.github.com>
2025-01-11 15:49:57 +0000

    .

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,6 +1,37 @@
 const coding=["js","ruby","cpp","swift","java"]
 coding.forEach( function (item) {
-    console.log(item)
+    // console.log(item)
 });
 
 //callback function ka name nhi hota
+coding.forEach((item)=>{
+// console.log(item)
+})
+
+function printMe(item){
+    // console.log(item);
+}
+// coding.forEach(printMe)
+
+coding.forEach((item,index,arr)=>{
+    // console.log(item,index,arr)
+})
+
+const myCoding = [{
+    languageName:"javascript",
+    languageFileName:"js"
+},
+{
+    languageName:"c++",
+    languageFileName:"cpp"
+},
+{
+    languageName:"java",
+    languageFileName:"java"
+}
+]
+
+//Database se value array ki form me aati hai or har ek value object hoti hai
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+})
